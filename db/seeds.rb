d3a1790e02085a30559f3d907d8e8ebe5d598913
davidbattersby <info@davidbattersby.com>
2022-11-28 14:41:26 +0800

    add database seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create([
+  { username: "sarah", email: "sarah@example.com", password: "Password123" },
+  { username: "jane", email: "jane@example.com", password: "Password123" },
+  { username: "mark", email: "mark@example.com", password: "Password123" },
+  { username: "john", email: "john@example.com", password: "Password123" }
+])
