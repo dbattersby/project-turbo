8bdd37307fe4c2ecf48f436ef047b6bfd1cba054
davidbattersby <info@davidbattersby.com>
2022-11-28 20:05:15 +0800

    add basic validation to project model, add enum for project status

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  enum status: { draft: 0, published: 1, completed: 2, archived: 3 }
 end
