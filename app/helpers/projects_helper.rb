515294323ddb97a1e59d31bb355e45a495f34e59
davidbattersby <info@davidbattersby.com>
2022-11-28 20:06:42 +0800

    update styling for project views, add helper method for due date

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def project_due_date project
+    if project.due_date.present?
+      Time.now > project.due_date ? "Project overdue by #{ time_ago_in_words(project.due_date) }" : "Due in #{ time_ago_in_words(project.due_date) } from now"
+    else
+      "No due date set for this project"
+    end
+  end
 end
