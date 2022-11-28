d86f9195aec722918b66db1561955f3da98f4c56
davidbattersby <info@davidbattersby.com>
2022-11-28 14:11:51 +0800

    add controller for pages, set default root for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'pages/about', as: :about
+  get 'pages/features', as: :features
+  get 'pages/contact', as: :contact
+
+  root "pages#home"
 end
