5b59d88855adcc70b869720ba56a687521030500
davidbattersby <info@davidbattersby.com>
2022-11-29 11:34:28 +0800

    add ability to edit tasks within modal and update without page refresh

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :set_project_and_task, only: %i[ show edit update destroy ]
 
   def index
     @tasks = Task.all
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
     @project = Project.find(params[:project_id])
     @task = Task.new
   end
-
+  
   def edit
   end
 
@@ -34,7 +34,10 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to @task, notice: "Task was successfully updated."
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(helpers.dom_id(@task), partial: "tasks/task", locals: { task: @task }) }
+        format.html { redirect_to @task, notice: "Task was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -65,7 +68,8 @@ class TasksController < ApplicationController
   end
 
   private
-    def set_task
+    def set_project_and_task
+      @project = Project.find(params[:project_id])
       @task = Task.find(params[:id])
     end
 
