ab6d5280d2a36f8702338b5fb3f3b972e0278f7c
davidbattersby <info@davidbattersby.com>
2022-11-29 13:20:38 +0800

    move project summary to partial and replace when add/edit/delete, add before action for loading project in tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
-  before_action :set_project_and_task, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :set_project, only: %i[ new edit update create destroy ]
 
   def index
     @tasks = Task.all
@@ -9,7 +11,6 @@ class TasksController < ApplicationController
   end
 
   def new
-    @project = Project.find(params[:project_id])
     @task = Task.new
   end
   
@@ -19,16 +20,13 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.project_id = params[:project_id]
-
+    
     if @task.save
-      set_totals()
-
       respond_to do |format|
         format.turbo_stream
         format.html { redirect_to @task, notice: "Task was successfully created." }
       end
     else
-      @project = @task.project
       render :new, status: :unprocessable_entity
     end
   end
@@ -36,7 +34,7 @@ class TasksController < ApplicationController
   def update
     if @task.update(task_params)
       respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.replace(helpers.dom_id(@task), partial: "tasks/task", locals: { task: @task }) }
+        format.turbo_stream
         format.html { redirect_to @task, notice: "Task was successfully updated." }
       end
     else
@@ -46,7 +44,6 @@ class TasksController < ApplicationController
 
   def destroy
     if @task.destroy
-      set_totals()
       respond_to do |format|
         format.turbo_stream
         format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
@@ -55,14 +52,12 @@ class TasksController < ApplicationController
   end
 
   private
-    def set_project_and_task
-      @project = Project.find(params[:project_id])
+    def set_task
       @task = Task.find(params[:id])
     end
 
-    def set_totals
-      @total_tasks_outstanding = Task.where(project_id: params[:project_id], status: :to_do).size
-      @total_tasks_completed = Task.where(project_id: params[:project_id], status: :complete).size
+    def set_project
+      @project = Project.find(params[:project_id])
     end
 
     def task_params
